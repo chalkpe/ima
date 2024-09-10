0a25e9f9a6a5639baf3acc01d3d8eb09266fa78c
Chalk <9481405+chalkpe@users.noreply.github.com>
2024-09-10 22:46:20 +0900

    feat: remove proxy, add env

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,13 +5,4 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  server: {
-    proxy: {
-      '/server': {
-        ws: true,
-        rewriteWsOrigin: true,
-        target: 'ws://localhost:5172',
-      },
-    },
-  },
 })

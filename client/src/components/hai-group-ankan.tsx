9048b6fb9eb364248fe4689cf2954efd65358c1a
Chalk <chalk@chalk.pe>
2024-09-25 00:08:46 +0900

    feat: framer motion to ankan

--- client/src/components/hai-group-ankan.tsx
+++ client/src/components/hai-group-ankan.tsx
@@ -11,21 +11,20 @@ interface HaiGroupAnkanProps {
 }
 
 const HaiGroupAnkan: FC<HaiGroupAnkanProps> = ({ set, size, flip, animate }) => {
+  const tiles = [...set.tiles].sort(
+    (a, b) =>
+      ankanAttributeOrder.indexOf(a.attribute) - ankanAttributeOrder.indexOf(b.attribute) ||
+      ankanBackgroundOrder.indexOf(a.background) - ankanBackgroundOrder.indexOf(b.background) ||
+      a.index - b.index
+  )
+
   return (
     <>
-      <Hai size={size} tile={backTile} />
-      {[...set.tiles]
-        .sort(
-          (a, b) =>
-            ankanAttributeOrder.indexOf(a.attribute) - ankanAttributeOrder.indexOf(b.attribute) ||
-            ankanBackgroundOrder.indexOf(a.background) - ankanBackgroundOrder.indexOf(b.background) ||
-            a.index - b.index
-        )
-        .slice(0, 2)
-        .map((tile) => (
-          <Hai key={tile.index} size={size} tile={tile} animate={animate} flip={flip} />
-        ))}
-      <Hai size={size} tile={backTile} />
+      <Hai key={tiles[2].index} size={size} tile={{ ...backTile, index: tiles[2].index }} animate />
+      {tiles.slice(0, 2).map((tile) => (
+        <Hai key={tile.index} size={size} tile={tile} animate={animate} flip={flip} />
+      ))}
+      <Hai key={tiles[3].index} size={size} tile={{ ...backTile, index: tiles[3].index }} animate />
     </>
   )
 }
